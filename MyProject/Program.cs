495207249e4e85bb0d16973769e7dd4698ddce08
ParsaValii <parsa.valizadee@gmail.com>
2023-12-05 00:11:13 +0330

    nt

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,8 +1,16 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Service.Interfaces;
+using Service.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ParsaDbContext>(
+        options => options.UseSqlServer("Data Source=FlowAley;Initial Catalog=ParsaDb;Integrated Security=True;Trust Server Certificate=True"));
+builder.Services.AddScoped<IPageGroup, PageGroupService>();
+builder.Services.AddScoped<IPage, PageService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,8 +28,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 app.MapControllerRoute(
-            name: "areas",
-            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
